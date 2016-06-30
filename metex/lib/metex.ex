e3e18c8b028b2eed1da787a88e1b6d2af8c296dd
Jeff Smith <jeffmsmithdev@gmail.com>
2016-06-29 21:34:57 -0400

    on to ch4

---
+++ metex/lib/metex.ex
@@ -0,0 +1,25 @@
+# defmodule Metex do
+#
+#   def temperatures_of(cities) do
+#     coordinator_pid = spawn(Metex.Coordinator, :loop, [[],
+#     Enum.count(cities)])
+#
+#       cities |> Enum.each fn(city ->
+#         worker_pid = spawn(Metex.Worker, :loop, [])
+#       end)
+#   end
+#
+# end
+
+defmodule Metex do
+
+  def temperatures_of(cities) do
+    coordinator_pid = spawn(Metex.Coordinator, :loop, [])
+
+    cities |> Enum.each(fn city->
+      worker_pid = spawn(Metex.Worker, :loop, [])
+      send worker_pid, {coordinator_pid, city}
+    end)
+  end
+
+end
