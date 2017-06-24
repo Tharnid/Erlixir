f550346f0b7080b654d6cb3c8f51d0bb1ec2862c
Tharnid <jeffmsmithdev@gmail.com>
2017-06-24 19:09:06 -0400

    bad commit its broke

--- TCG/hangman/test/game_test.exs
+++ TCG/hangman/test/game_test.exs
@@ -1,14 +1,45 @@
 defmodule GameTest do
-  use ExUnit.case
+  use ExUnit.Case
 
   alias Hangman.Game
 
   test "new_game returns structure" do
     game = Game.new_game()
 
-    assert game.turns_left == 1
+    assert game.turns_left == 7
     assert game.game_state == :initializing
     assert length(game.letters) > 0
+  end
+
+  # test "state isn't changed for :won game" do
+  #   game = Game.new_game() |>  Map.put(:game_state, :won)
+  #   assert { ^game, _ } = Game.make_move(game, "x")
+  # end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [ :won, :lost ] do
+      game = Game.new_game() |>  Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, "x")
+    end
+  end
+
+  # test "state isn't changed for :lost game" do
+  #   game = Game.new_game() |>  Map.put(:game_state, :lost)
+  #   assert { ^game, _ } = Game.make_move(game, "x")
+  # end
+
+  test "first occurence of letter is not already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
+
+  test "second ocurrence of letter is not already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :already_used
   end
 
 end
