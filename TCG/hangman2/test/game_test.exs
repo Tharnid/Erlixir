8ad42e4b6f0f36ed8ffeba3b9ae184304a2a5b53
Tharnid <jeffmsmithdev@gmail.com>
2017-06-26 23:40:22 -0400

    pattern matched game state

--- TCG/hangman2/test/game_test.exs
+++ TCG/hangman2/test/game_test.exs
@@ -10,4 +10,22 @@ defmodule GameTest do
     assert game.game_state == :initializing
     assert length(game.letters) > 0
   end
+
+  test "sate isn't changedc ro :won or :lost game" do
+    for state <- [ :won, :lost ] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert { ^game, _ } = Game.make_move(game, "x")
+    end
+  end
+
+  #test "sate isn't changedc ro :lost game" do
+  #  game = Game.new_game()
+  #    |> Map.put(:game_state, :lost)
+    #{ new_game, _} = Game.make_move(game, "x")
+    # assert new_game == game
+  #  assert { ^game, _ } = Game.make_move(game, "x")
+  # end
+
+
+
 end
