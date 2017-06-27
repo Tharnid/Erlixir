9019be8e27ca4a8087b787623d1a448517dfc4c9
Tharnid <jeffmsmithdev@gmail.com>
2017-06-27 13:22:57 -0400

    on to good move

--- TCG/hangman2/test/game_test.exs
+++ TCG/hangman2/test/game_test.exs
@@ -26,6 +26,18 @@ defmodule GameTest do
   #  assert { ^game, _ } = Game.make_move(game, "x")
   # end
 
+  test "first occurence of letter is not already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
 
+  test "second occurence of letter is not already used" do
+    game = Game.new_game()
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    { game, _tally } = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
 
 end
