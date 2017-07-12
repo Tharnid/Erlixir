3438bc95b57a56ef1901f44aee3183be4e721ef0
Tharnid <jeffmsmithdev@gmail.com>
2017-07-11 22:13:17 -0400

    sent some messages

--- TCG/procMS/procs.exs
+++ TCG/procMS/procs.exs
@@ -1,10 +1,16 @@
 defmodule Procs do
 
-  def greeter(name) do
-    Process.sleep(1000)
-    IO.puts "Hello #{name}"
+  def greeter() do
+    receive do
+      msg ->
+        IO.puts "Hello #{inspect msg}"
+    end
+    # Process.sleep(1000)
+    greeter()
   end
 
 end
 
 # spawn Procs, :greeter, [ "world" ]
+
+# when you exit the pid dies
