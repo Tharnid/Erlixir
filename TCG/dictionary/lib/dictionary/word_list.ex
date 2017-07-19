7f4f064c0fb3ff4bbe01c2cf5c54a749db384428
Tharnid <jeffmsmithdev@gmail.com>
2017-07-19 17:28:16 -0400

    changed Dictionary to an Agent

--- TCG/dictionary/lib/dictionary/word_list.ex
+++ TCG/dictionary/lib/dictionary/word_list.ex
@@ -1,9 +1,12 @@
 defmodule Dictionary.WordList do
 
-  def random_word(word_list) do # takes param I believe
-    #Enum.random(word_list())
-    word_list
-    |> Enum.random()
+  def start_link() do
+    Agent.start_link(&word_list/0)
+  end
+
+  def random_word(agent) do # takes param I believe
+    # |> Enum.random()
+    Agent.get(agent, &Enum.random/1) # list of words
   end
 
   def word_list do
