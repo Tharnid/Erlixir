ecbb811df347f288a9a0d23f7300e5922b0316d9
Tharnid <jeffmsmithdev@gmail.com>
2017-09-29 14:03:29 -0400

    on to gallows

--- TCG/ep4Phx/gallows/lib/gallows_web/controllers/page_controller.ex
+++ TCG/ep4Phx/gallows/lib/gallows_web/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule GallowsWeb.PageController do
   use GallowsWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    # my_assigns = [ produce: "orange", count: 3 ]
+    my_assigns = %{ produce: "date", count: 3 }
+    render conn, "index.html", my_assigns
+    #, produce: "orange", count: 3
   end
 end
