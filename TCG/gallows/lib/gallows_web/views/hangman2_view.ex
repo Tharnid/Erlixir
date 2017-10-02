689c432937d3b39ad633bc5209e793bbef6e6545
Tharnid <jeffmsmithdev@gmail.com>
2017-10-01 22:57:55 -0400

    done with form helper

--- TCG/gallows/lib/gallows_web/views/hangman2_view.ex
+++ TCG/gallows/lib/gallows_web/views/hangman2_view.ex
@@ -1,3 +1,37 @@
 defmodule GallowsWeb.Hangman2View do
   use GallowsWeb, :view
+
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
+  def new_game_button(conn) do
+    button("New Game", to: hangman2_path(conn, :create_game ))
+  end
+
+  # responses correspond with BS classes
+  @responses %{
+    :won => { :success, "You Won!!!" },
+    :lost => { :danger, "You Lost!!!" },
+    :good_guess => { :success, "Good Guess!!!" },
+    :bad_guess => { :warning, "Bad Guess!!!" },
+    :already_used => { :info, "You already guessed that!!!" },
+  }
+
+  # game state
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  defp alert(nil), do: ""
+  defp alert({class, message}) do
+    """
+    <div class="alert alert-#{class}">
+      #{message}
+    </div>
+    """
+    |> raw()
+  end
+
 end
