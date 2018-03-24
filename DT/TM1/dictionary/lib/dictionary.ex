e59bb610ba9fc618c55177c7c964aeb173a8c1e0
Tharnid <jeffmsmithdev@gmail.com>
2018-03-24 19:02:20 -0400

    done with first dictionary

---
+++ DT/TM1/dictionary/lib/dictionary.ex
@@ -0,0 +1,15 @@
+defmodule Dictionary do
+  # def hello do
+  #   IO.puts "Hello from Elixir!"
+  # end
+
+  def random_word() do
+    Enum.random(word_list())
+  end
+
+  def word_list do
+    contents = File.read!("assets/words.txt")
+    list     = String.split(contents, ~r/\n/)
+  end
+end
+
