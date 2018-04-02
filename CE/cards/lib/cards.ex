f33281b7f13fcaa41c24d70ba6b9587f72c179d2
Tharnid <jeffmsmithdev@gmail.com>
2018-04-02 13:19:53 -0400

    on to complex functions

--- CE/cards/lib/cards.ex
+++ CE/cards/lib/cards.ex
@@ -7,11 +7,26 @@ defmodule Cards do
 
   # list of playing cards
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "King", "Two", "Three", "Four", "Five"]
+    suits = ["Spaces", "Clubs", "Hearts", "Diamonds"]
+
+    # iterate over collection elements
+    # list comprehension
+    for suit <- suits do # essentially a mapping function
+      "Is this a map?"
+    end
   end
   # from iex recompile or r...c
   # iex -S mix
 
-  
+  # arity number of arguments a function excepts shuffle/1 (one argument etc)
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do # ? return T or F...boolean 
+    Enum.member?(deck, card)
+  end
 
 end
